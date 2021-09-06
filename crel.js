b955ab396e02d782e5f36b0c5825c2297f179aec
patrickhaussmann <git@patrickhaussmann.de>
2021-09-06 21:46:28 +0200

    Autoformat

--- crel.js
+++ crel.js
@@ -9,115 +9,132 @@ This might make it harder to read at times, but the code's intention should be t
 
 // IIFE our function
 ((exporter) => {
-    // Define our function and its properties
-    // These strings are used multiple times, so this makes things smaller once compiled
-    const func = 'function',
-        isNodeString = 'isNode',
-        proxyString = 'proxy',
-        tagTransformString = 'tagTransform',
-        doc = document,
-        hasOwnProperty = Object.hasOwnProperty.call,
-        // Helper functions used throughout the script
-        isType = (object, type) => typeof object === type,
-        // Recursively appends children to given element. As a text node if not already an element
-        appendChild = (element, child) => {
-            if (child !== null) {
-                if (Array.isArray(child)) { // Support (deeply) nested child elements
-                    child.map(subChild => appendChild(element, subChild));
-                } else {
-                    if (!crel[isNodeString](child)) {
-                        child = doc.createTextNode(child);
-                    }
-                    element.appendChild(child);
-                }
-            }
-        },
+  // Define our function and its properties
+  // These strings are used multiple times, so this makes things smaller once compiled
+  const func = "function",
+    isNodeString = "isNode",
+    proxyString = "proxy",
+    tagTransformString = "tagTransform",
+    doc = document,
+    hasOwnProperty = Object.hasOwnProperty.call,
+    // Helper functions used throughout the script
+    isType = (object, type) => typeof object === type,
+    // Recursively appends children to given element. As a text node if not already an element
+    appendChild = (element, child) => {
+      if (child !== null) {
+        if (Array.isArray(child)) {
+          // Support (deeply) nested child elements
+          child.map((subChild) => appendChild(element, subChild));
+        } else {
+          if (!crel[isNodeString](child)) {
+            child = doc.createTextNode(child);
+          }
+          element.appendChild(child);
+        }
+      }
+    },
     // Define our function as a proxy interface
-    crel = new Proxy((element, ...children) => {
+    crel = new Proxy(
+      (element, ...children) => {
         // Define all used variables / shortcuts here, to make things smaller once compiled
         let settings = children[0],
-            key,
-            attribute;
+          key,
+          attribute;
         // If first argument is an element, use it as is, otherwise treat it as a tagname
-        element = crel.isElement(element) ? element : doc.createElement(element);
+        element = crel.isElement(element)
+          ? element
+          : doc.createElement(element);
         // Check if second argument is a settings object
-        if (isType(settings, 'object') && !crel[isNodeString](settings) && !Array.isArray(settings)) {
-            // Don't treat settings as a child
-            children.shift();
-            // Go through settings / attributes object, if it exists
-            for (key in settings) {
-                // Store the attribute into a variable, before we potentially modify the key
-                attribute = settings[key];
-                // Get mapped key / function, if one exists
-                key = crel.attrMap[key] || key;
-                // Note: We want to prioritise mapping over properties
-                if (isType(key, func)) {
-                    key(element, attribute);
-                } else if (isType(attribute, func)) { // ex. onClick property
-                    element[key] = attribute;
-                } else {
-                    // Set the element attribute
-                    element.setAttribute(key, attribute);
-                }
+        if (
+          isType(settings, "object") &&
+          !crel[isNodeString](settings) &&
+          !Array.isArray(settings)
+        ) {
+          // Don't treat settings as a child
+          children.shift();
+          // Go through settings / attributes object, if it exists
+          for (key in settings) {
+            // Store the attribute into a variable, before we potentially modify the key
+            attribute = settings[key];
+            // Get mapped key / function, if one exists
+            key = crel.attrMap[key] || key;
+            // Note: We want to prioritise mapping over properties
+            if (isType(key, func)) {
+              key(element, attribute);
+            } else if (isType(attribute, func)) {
+              // ex. onClick property
+              element[key] = attribute;
+            } else {
+              // Set the element attribute
+              element.setAttribute(key, attribute);
             }
+          }
         }
         // Append remaining children to element and return it
         appendChild(element, children);
         return element;
-    }, {// Binds specific tagnames to crel function calls with that tag as the first argument
+      },
+      {
+        // Binds specific tagnames to crel function calls with that tag as the first argument
         get: (target, key) => {
-            if (key in target) {
-                return target[key];
-            }
-            key = target[tagTransformString](key);
-            if (!(key in target[proxyString])) {
-                target[proxyString][key] = target.bind(null, key);
-            }
-            return target[proxyString][key];
-        }
-    });
-
-    // Used for mapping attribute keys to supported versions in bad browsers, or to custom functionality
-    crel.attrMap = {
-        "on": (element, value) => {
-            for (let eventName in value) {
-                if (hasOwnProperty(value, eventName)) {
-                    element.addEventListener(eventName, value[eventName]);
-                }
-            }
+          if (key in target) {
+            return target[key];
+          }
+          key = target[tagTransformString](key);
+          if (!(key in target[proxyString])) {
+            target[proxyString][key] = target.bind(null, key);
+          }
+          return target[proxyString][key];
         },
-        "style": (element, value) => {
-            for (let styleName in value) {
-                if (hasOwnProperty(value, styleName)) {
-                    element.style[styleName] = value[styleName];
-                }
-            }
-        }
-      };
+      }
+    );
 
-    crel.isElement = object => object instanceof Element;
-    crel[isNodeString] = node => node instanceof Node;
-    // Bound functions are "cached" here for legacy support and to keep Crels internal structure clean
-    crel[proxyString] = new Proxy((...args)=>{crel(...args)}, { 
-        get: (target, key) => {
-            if (key == proxyString) return;
-            return target[key] || crel[key];
+  // Used for mapping attribute keys to supported versions in bad browsers, or to custom functionality
+  crel.attrMap = {
+    on: (element, value) => {
+      for (let eventName in value) {
+        if (hasOwnProperty(value, eventName)) {
+          element.addEventListener(eventName, value[eventName]);
         }
-    });
-    // Transforms tags on call, to for example allow dashes in tags
-    crel[tagTransformString] = key => key;
-    
-    // Export crel
-    exporter(crel, func);
-})((product, func) => {
-    if (typeof exports === 'object') {
-        // Export for Browserify / CommonJS format
-        module.exports = product;
-    } else if (typeof define === func && define.amd) {
-        // Export for RequireJS / AMD format
-        define(() => product);
-    } else {
-        // Export as a 'global' function
-        this.crel = product;
+      }
+    },
+    style: (element, value) => {
+      for (let styleName in value) {
+        if (hasOwnProperty(value, styleName)) {
+          element.style[styleName] = value[styleName];
+        }
+      }
+    },
+  };
+
+  crel.isElement = (object) => object instanceof Element;
+  crel[isNodeString] = (node) => node instanceof Node;
+  // Bound functions are "cached" here for legacy support and to keep Crels internal structure clean
+  crel[proxyString] = new Proxy(
+    (...args) => {
+      crel(...args);
+    },
+    {
+      get: (target, key) => {
+        if (key == proxyString) return;
+        return target[key] || crel[key];
+      },
     }
+  );
+  // Transforms tags on call, to for example allow dashes in tags
+  crel[tagTransformString] = (key) => key;
+
+  // Export crel
+  exporter(crel, func);
+})((product, func) => {
+  if (typeof exports === "object") {
+    // Export for Browserify / CommonJS format
+    module.exports = product;
+  } else if (typeof define === func && define.amd) {
+    // Export for RequireJS / AMD format
+    define(() => product);
+  } else {
+    // Export as a 'global' function
+    this.crel = product;
+  }
 });
