PatrickHaussmann/crel

commit d4efdd4b4768ef3625f3b1738ace330f736048a9

Simplify if-statement

crel.js
@@ -50,11 +50,7 @@ This might make it harder to read at times, but the code's intention should be t
           key,
           attribute;
         // Check if second argument is a settings object
-        if (
-          isType(settings, "object") &&
-          !crel[isNodeString](settings) &&
-          !Array.isArray(settings)
-        ) {
+        if (isType(settings, "object")) {
           // Don't treat settings as a child
           children.shift();
           // Go through settings / attributes object, if it exists

commit b9fbf9e6bfe2e9201090671cde9c9986c82ca567

revert: Simplify if-statement d4efdd4b4768ef3625f3b1738ace330f736048a9 introduced a bug: if no settings object is passed, then the attributes of thechild to be append are read as a setting

crel.js
@@ -48,7 +48,11 @@ This might make it harder to read at times, but the code's intention should be t
           key,
           attribute;
         // Check if second argument is a settings object
-        if (isType(settings, "object")) {
+        if (
+          isType(settings, "object") &&
+          !isNode(settings) &&
+          !Array.isArray(settings)
+        ) {
           // Don't treat settings as a child
           children.shift();
           // Go through settings / attributes object, if it exists